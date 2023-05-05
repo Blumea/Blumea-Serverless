8195ec413e31165d509abd37cc47b0b568f45b54
Akash Chouhan <akash.c1500@gmail.com>
2023-05-05 12:45:18 +0530

    :wrench: rate limter & response sd config update

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const expressLimit = require('express-rate-limit');
 
 let rateLimiter = expressLimit({
     windowMs: 60 * 1000, // 1min window
-    max: 20, // Limit each IP to 20 requests per window.
+    max: 40, // Limit each IP to 40 requests per window.
     standardHeaders: true,
     legacyHeaders: false,
 });
